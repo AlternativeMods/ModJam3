ad517d6bceb750acc981057d6329edd90283f13d
Lordmau5 <dustin.hecken@web.de>
2013-12-13 16:23:45 +0100

    Basic work on the radio block

--- src/main/java/jkmau5/modjam/radiomod/RadioMod.java
+++ src/main/java/jkmau5/modjam/radiomod/RadioMod.java
@@ -1,9 +1,34 @@
 package jkmau5.modjam.radiomod;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+import jkmau5.modjam.radiomod.block.BlockRadio;
+import jkmau5.modjam.radiomod.tile.TileEntityRadio;
+import net.minecraft.creativetab.CreativeTabs;
 
 @Mod(modid = RadioMod.MODID)
 public class RadioMod {
 
     public static final String MODID = "RadioMod";
+    public BlockRadio blockRadio;
+
+    public CreativeTabs tabRadioMod = new CreativeTabs("Radio Mod") {
+
+    };
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        blockRadio = new BlockRadio(2500);
+        GameRegistry.registerBlock(blockRadio, "BlockRadio");
+
+        GameRegistry.registerTileEntity(TileEntityRadio.class, "TileRadio");
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        LanguageRegistry.addName(blockRadio, "Radio Block");
+    }
 }
---
+++ src/main/java/jkmau5/modjam/radiomod/tile/TileEntityRadio.java
@@ -0,0 +1,17 @@
+package jkmau5.modjam.radiomod.tile;
+
+import net.minecraft.tileentity.TileEntity;
+
+/**
+ * Author: Lordmau5
+ * Date: 13.12.13
+ * Time: 16:05
+ * You are allowed to change this code,
+ * however, not to publish it without my permission.
+ */
+public class TileEntityRadio extends TileEntity {
+
+    public TileEntityRadio() {
+
+    }
+}
