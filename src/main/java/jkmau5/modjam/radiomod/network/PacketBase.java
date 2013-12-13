608cde330909001d6147bd8791cc2a1da3d17dc8
jk-5 <jeffreykog@me.com>
2013-12-13 17:06:26 +0100

    Basic network stuff and added a logger

---
+++ src/main/java/jkmau5/modjam/radiomod/network/PacketBase.java
@@ -0,0 +1,70 @@
+package jkmau5.modjam.radiomod.network;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import cpw.mods.fml.common.network.PacketDispatcher;
+import jkmau5.modjam.radiomod.RMLogger;
+import jkmau5.modjam.radiomod.RadioMod;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.network.packet.Packet250CustomPayload;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * No description given
+ *
+ * @author jk-5
+ */
+public abstract class PacketBase {
+
+    private static BiMap<Integer, Class<? extends PacketBase>> packets = HashBiMap.create();
+
+    private static void registerPacket(int id, Class<? extends PacketBase> cl){
+        packets.put(id, cl);
+    }
+
+    static {
+        registerPacket(0, PacketOpenGui.class);
+    }
+
+    public final Packet250CustomPayload getPacket(){
+        Packet250CustomPayload ret = null;
+        ByteArrayDataOutput output = ByteStreams.newDataOutput();
+        try{
+            output.writeByte(this.getID());
+            this.writePacket(output);
+            ret = PacketDispatcher.getPacket(RadioMod.MODID, output.toByteArray());
+        }catch(IOException e){
+            RMLogger.severe(e, "Error while writing packet data for packet " + this.getID());
+        }
+        return ret;
+    }
+
+    public static PacketBase readPacket(Packet250CustomPayload packet, EntityPlayer player){
+        PacketBase ret = null;
+        ByteArrayDataInput input = ByteStreams.newDataInput(packet.data);
+        try{
+            int packetid = input.readByte();
+            PacketBase newPacket = packets.get(packetid).newInstance();
+            if(newPacket != null){
+                newPacket.readPacket(input);
+            }
+            ret = newPacket;
+        }catch(Exception e){
+            RMLogger.severe(e, "Error while reading packet");
+        }
+        return ret;
+    }
+
+    public final int getID(){
+        return packets.inverse().get(this.getClass());
+    }
+
+    public abstract void writePacket(DataOutput output) throws IOException;
+    public abstract void readPacket(DataInput input) throws IOException;
+}
---
+++ src/main/java/jkmau5/modjam/radiomod/network/PacketOpenGui.java
@@ -0,0 +1,10 @@
+package jkmau5.modjam.radiomod.network;
+
+/**
+ * No description given
+ *
+ * @author jk-5
+ */
+public class PacketOpenGui extends PacketBase {
+
+}
