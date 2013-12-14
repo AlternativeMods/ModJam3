0c211a41e6724ac64fe7bb5c260b212bc297bcf3
Lordmau5 <dustin.hecken@web.de>
2013-12-14 14:52:55 +0100

    commit from 12/14/13 14:52:55. 0 deletions, 3 adds, 1 modifications

---
+++ src/main/java/jkmau5/modjam/radiomod/block/BlockCable.java
@@ -0,0 +1,20 @@
+package jkmau5.modjam.radiomod.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+/**
+ * Author: Lordmau5
+ * Date: 14.12.13
+ * Time: 14:52
+ * You are allowed to change this code,
+ * however, not to publish it without my permission.
+ */
+public class BlockCable extends Block {
+
+    public BlockCable(int par1) {
+        super(par1, Material.iron);
+    }
+
+
+}
--- src/main/java/jkmau5/modjam/radiomod/client/ProxyClient.java
+++ src/main/java/jkmau5/modjam/radiomod/client/ProxyClient.java
@@ -1,8 +1,11 @@
 package jkmau5.modjam.radiomod.client;
 
 import cpw.mods.fml.client.registry.RenderingRegistry;
+import jkmau5.modjam.radiomod.Constants;
 import jkmau5.modjam.radiomod.client.render.RenderBlockAntenna;
 import jkmau5.modjam.radiomod.server.ProxyCommon;
+import net.minecraftforge.client.model.AdvancedModelLoader;
+import net.minecraftforge.client.model.IModelCustom;
 
 /**
  * No description given
@@ -12,11 +15,13 @@ import jkmau5.modjam.radiomod.server.ProxyCommon;
 public class ProxyClient extends ProxyCommon {
 
     public static final int renderID_Antenna = RenderingRegistry.getNextAvailableRenderId();
+    public static IModelCustom blockAntenna;
 
     @Override
     public void init(){
         super.init();
 
         RenderingRegistry.registerBlockHandler(new RenderBlockAntenna());
+        blockAntenna = AdvancedModelLoader.loadModel(Constants.MODID + ":/models/Antenna.tcn");
     }
 }
