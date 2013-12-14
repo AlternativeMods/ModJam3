afa370ca41bd25a3c1f96a38dfdef3d32ad7c78a
Lordmau5 <dustin.hecken@web.de>
2013-12-14 15:23:02 +0100

    commit from 12/14/13 15:23:02. 0 deletions, 1 adds, 3 modifications

--- src/main/java/jkmau5/modjam/radiomod/block/BlockCable.java
+++ src/main/java/jkmau5/modjam/radiomod/block/BlockCable.java
@@ -1,11 +1,21 @@
 package jkmau5.modjam.radiomod.block;
 
+import jkmau5.modjam.radiomod.Constants;
+import jkmau5.modjam.radiomod.RadioMod;
 import jkmau5.modjam.radiomod.client.ProxyClient;
 import jkmau5.modjam.radiomod.tile.TileEntityCable;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 /**
  * Author: Lordmau5
@@ -18,6 +28,13 @@ public class BlockCable extends Block {
 
     public BlockCable(int par1) {
         super(par1, Material.iron);
+        setCreativeTab(RadioMod.tabRadioMod);
+        setUnlocalizedName("blockCable");
+    }
+
+    public void registerIcons(IconRegister par1IconRegister)
+    {
+        this.blockIcon = par1IconRegister.registerIcon(Constants.MODID.toLowerCase() + ":" + getUnlocalizedName().substring(5));
     }
 
     public boolean hasTileEntity(int metadata) {
@@ -33,6 +50,18 @@ public class BlockCable extends Block {
         return ProxyClient.renderID_Cable;
     }
 
+    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase ent, ItemStack is) {
+        super.onBlockPlacedBy(world, x, y, z, ent, is);
+        if(!world.isRemote)
+            return;
+
+        TileEntityCable cable = (TileEntityCable) world.getBlockTileEntity(x, y, z);
+        if(cable == null)
+            return;
+
+        cable.onNeighborTileChange();
+    }
+
     public void onNeighborTileChange(World world, int x, int y, int z, int tileX, int tileY, int tileZ) {
         TileEntityCable cable = (TileEntityCable) world.getBlockTileEntity(x, y, z);
         if(cable == null)
@@ -40,5 +69,65 @@ public class BlockCable extends Block {
 
         if(world.isRemote)
             cable.onNeighborTileChange();
+    }
+
+    public void setBlockBoundsBasedOnState(IBlockAccess world, int x, int y, int z) {
+        setCableBoundingBox(world, x, y, z);
+    }
+
+    public void setCableBoundingBox(IBlockAccess world, int x, int y, int z) {
+        float minX = 0.4F;
+        float minY = 0.4F;
+        float minZ = 0.4F;
+
+        float maxX = 0.6F;
+        float maxY = 0.6F;
+        float maxZ = 0.6F;
+
+        if(isCableAtPosition(world, x - 1, y, z))
+            minX = 0F;
+        if(isCableAtPosition(world, x + 1, y, z))
+            maxX = 1F;
+
+        if(isCableAtPosition(world, x, y - 1, z))
+            minY = 0F;
+        if(isCableAtPosition(world, x, y + 1, z))
+            maxY = 1F;
+
+        if(isCableAtPosition(world, x, y, z - 1))
+            minZ = 0F;
+        if(isCableAtPosition(world, x, y, z + 1))
+            maxZ = 1F;
+
+        this.setBlockBounds(minX, minY, minZ, maxX, maxY, maxZ);
+    }
+
+    public void addCollisionBoxesToList(World world, int x, int y, int z, AxisAlignedBB axisAlignedBB, List list, Entity entity) {
+        setCableBoundingBox(world, x, y, z);
+        AxisAlignedBB aabb = super.getCollisionBoundingBoxFromPool(world, x, y, z);
+        if ((aabb != null) && (axisAlignedBB.intersectsWith(aabb))) {
+            list.add(aabb);
+        }
+    }
+
+    public boolean isCableAtPosition(IBlockAccess world, int x, int y, int z) {
+        TileEntity tile = world.getBlockTileEntity(x, y, z);
+        if(tile == null)
+            return false;
+        if(tile instanceof TileEntityCable)
+            return true;
+        return false;
+    }
+
+    public boolean renderAsNormalBlock() {
+        return false;
+    }
+
+    public boolean isBlockNormalCube(World world, int x, int y, int z) {
+        return false;
+    }
+
+    public boolean isOpaqueCube() {
+        return false;
     }
 }
