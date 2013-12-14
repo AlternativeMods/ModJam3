5e20f7cfa97d6b995fa49feb67941c8fbd48554b
jk-5 <jeffreykog@me.com>
2013-12-14 18:58:21 +0100

    A bit more itemrenderer tweaking

--- src/main/java/jkmau5/modjam/radiomod/client/render/ItemRendererAntenna.java
+++ src/main/java/jkmau5/modjam/radiomod/client/render/ItemRendererAntenna.java
@@ -32,17 +32,17 @@ public class ItemRendererAntenna implements IItemRenderer {
                 break;
             case INVENTORY:
                 GL11.glScalef(0.65f, 0.65f, 0.65f);
-                GL11.glTranslatef(0, -0.7f, 0);
+                GL11.glTranslatef(-0.3f, -0.7f, 0);
                 ProxyClient.antennaRenderer.doRender(180, 0, 0, 0);
                 break;
             case EQUIPPED:
-                GL11.glRotatef(75, 0, 0, 1);
-                GL11.glRotatef(-30, 1, 0, 0);
-                GL11.glTranslatef(0.0f, 0.50f, 0.35f);
+                GL11.glRotatef(67, 0, 0, 1);
+                GL11.glRotatef(-37, 1, 0, 0);
+                GL11.glTranslatef(0.35f, -1.8f, -0.10f);
                 ProxyClient.antennaRenderer.doRender(0, 0, 0, 0);
                 break;
             case EQUIPPED_FIRST_PERSON:
-                GL11.glRotatef(10, 1, 0, 0);
+                GL11.glRotatef(10, 1, 0, 0.4f);
                 GL11.glScalef(0.65f, 0.65f, 0.65f);
                 GL11.glTranslatef(-0.4f, 0.50f, 0.35f);
                 ProxyClient.antennaRenderer.doRender(140, 0, 0, 0);
