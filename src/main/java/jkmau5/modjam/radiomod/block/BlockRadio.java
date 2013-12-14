e433b25db9427ca443c31f9e0eb7c8386b05159f
jk-5 <jeffreykog@me.com>
2013-12-14 13:34:35 +0100

    This should be true

--- src/main/java/jkmau5/modjam/radiomod/block/BlockRadio.java
+++ src/main/java/jkmau5/modjam/radiomod/block/BlockRadio.java
@@ -37,7 +37,7 @@ public class BlockRadio extends Block {
 
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
         if(world.isRemote)
-            return false;
+            return true;
 
         TileEntity tempTile = world.getBlockTileEntity(x, y, z);
         if(tempTile == null || !(tempTile instanceof TileEntityRadio))
