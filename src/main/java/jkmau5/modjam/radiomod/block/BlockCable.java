137f7d4911a8a1ec7d8784c4d765dc2b4add5ed4
Lordmau5 <dustin.hecken@web.de>
2013-12-15 12:34:32 +0100

    Swing the arm, my player!

--- src/main/java/jkmau5/modjam/radiomod/block/BlockCable.java
+++ src/main/java/jkmau5/modjam/radiomod/block/BlockCable.java
@@ -54,7 +54,7 @@ public class BlockCable extends Block {
 
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
         if(world.isRemote)
-            return false;
+            return true;
 
         TileEntity tempTile = world.getBlockTileEntity(x, y, z);
         if(tempTile == null || !(tempTile instanceof TileEntityCable))
